56f8675ff8fbc77239d76f34146b5997de34e1b2
JEISON MARULANDA ESTRADA <jemarulanda@ulasallista.edu.co>
2019-05-15 10:03:30 -0500

    initial commit

---
+++ CalcularExcell/calcular_excell.py
@@ -0,0 +1,54 @@
+from openpyxl import load_workbook
+
+FILE_PATH = 'operaciones.xlsx'
+SHEET = 'SUMA'
+
+WORKBOOK = load_workbook(FILE_PATH, read_only=True)
+SH = WORKBOOK[SHEET]
+
+SUMA = open('SUMA.txt', 'w')
+for num1, num2 in SH.iter_rows(min_row=2):
+    try:
+        resultado = int(num1.value+num2.value)
+    except:
+        resultado = 'Error'
+    SUMA.write(str(resultado)+"\n")
+SUMA.close()
+
+SHEET = 'RESTA'
+SH = WORKBOOK[SHEET]
+
+RESTA = open('RESTA.txt', 'w')
+for num1, num2 in SH.iter_rows(min_row=2):
+    try:
+        resultado = int(num1.value-num2.value)
+    except:
+        resultado = 'Error'
+    RESTA.write(str(resultado)+"\n")
+RESTA.close()
+
+SHEET = 'MULTIPLICACIÓN'
+SH = WORKBOOK[SHEET]
+
+MULTI = open('MULTIPLICACION.txt', 'w')
+for num1, num2 in SH.iter_rows(min_row=2):
+    try:
+        resultado = int(num1.value*num2.value)
+    except:
+        resultado = 'Error'
+    MULTI.write(str(resultado)+"\n")
+MULTI.close()
+
+SHEET = 'DIVISIÓN'
+SH = WORKBOOK[SHEET]
+
+DIVI = open('DIVISION.txt', 'w')
+for num1, num2 in SH.iter_rows(min_row=2):
+    try:
+        resultado = int(num1.value/num2.value)
+    except:
+        resultado = 'Error'
+    DIVI.write(str(resultado)+"\n")
+DIVI.close()
+
+print('OK')
---
+++ Palindromo/palindromo_jeison.py
@@ -0,0 +1,14 @@
+def palindromo(palabra):
+    palabra1 = palabra.lower().replace(" ", "")
+    palabra2 = ""
+    total = len(palabra1)
+    for x in palabra1:
+        total = total-1
+        palabra2 = palabra2 + palabra1[total]
+    if palabra1 == palabra2:
+        print("Si es palindromo")
+    else:
+        print("No es palindromo")
+print("Ingresa la frase para validar si es palindromo: ")
+INP = input()
+palindromo(INP)
